bcda74e65f8649c62440d521c1abf459354bf6ac
Muhammadshoh01 <rajabovmuhammadshoh009@gmail.com>
2023-09-16 12:41:50 +0500

    adding new components to main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
 <template>
 	<div>
-		<h1>Bosh sahifa</h1>
-		<img src="/img/tree.jpg" alt="" />
+		<welcomeVue />
+		<categoryList />
+		<cardListVue />
+		<plusListVue />
 	</div>
 </template>
+
+<script setup>
+import welcomeVue from '~/components/page/index/welcome.vue'
+import categoryList from '~/components/page/index/category-list.vue'
+import cardListVue from '~/components/page/index/card-list.vue'
+import plusListVue from '~/components/page/index/plus-list.vue'
+</script>
