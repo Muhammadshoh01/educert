9acdd2baafe8d1858febe7a4c4020d0def58a51a
Muhammadshoh01 <rajabovmuhammadshoh009@gmail.com>
2023-09-16 12:38:36 +0500

    update and finish welcome component

--- components/page/index/welcome.vue
+++ components/page/index/welcome.vue
@@ -1,5 +1,27 @@
-<template></template>
+<template>
+	<div class="welcome">
+		<div class="welcome__body">
+			<div class="container">
+				<h1>Zamonaviy kasbni <span>EduCert</span> orqali egallang</h1>
+				<p>
+					O’z kasbini mutaxassislari tomonidan shakllantirilgan kurslarimiz
+					orqali o’zingizga mos bilim va salohiyatni qulay holatda o’rganing
+				</p>
+				<div class="btns d-flex gap-30">
+					<nuxt-link to="/" class="btn rounded">Batafsil</nuxt-link>
+					<nuxt-link to="/" class="btn bordered">Barcha kurslarimiz</nuxt-link>
+				</div>
+			</div>
+			<img src="/img/main.png" />
+		</div>
+		<partners />
+	</div>
+</template>
 
-<script setup></script>
+<script setup>
+import partners from '~/components/page/index/partners.vue'
+</script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import '../../../public/styles/css/components/welcome.scss';
+</style>
