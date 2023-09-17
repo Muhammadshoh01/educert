f3299653fbb92b061647ec97f8e5ba5c3cb7c82f
Muhammadshoh01 <rajabovmuhammadshoh009@gmail.com>
2023-09-17 17:28:21 +0500

    adding components to main page

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,12 @@
 		<categoryList />
 		<cardListVue />
 		<plusListVue />
+		<quoteListVue />
+		<statListVue />
+		<teacherList />
+		<promo />
+		<newslist />
+		<consult />
 	</div>
 </template>
 
@@ -12,4 +18,10 @@ import welcomeVue from '~/components/page/index/welcome.vue'
 import categoryList from '~/components/page/index/category-list.vue'
 import cardListVue from '~/components/page/index/card-list.vue'
 import plusListVue from '~/components/page/index/plus-list.vue'
+import quoteListVue from '~/components/page/index/quote-list.vue'
+import statListVue from '~/components/page/index/stat-list.vue'
+import teacherList from '~/components/page/index/teacher-list.vue'
+import promo from '~/components/page/index/promo.vue'
+import consult from '~/components/page/index/consult.vue'
+import newslist from '~/components/page/index/news-list.vue'
 </script>
