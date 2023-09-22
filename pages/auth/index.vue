7e1f664fe1803472d183c452329b987ddf8ed0d2
Muhammadshoh01 <rajabovmuhammadshoh009@gmail.com>
2023-09-22 23:13:20 +0500

    create auth layout

---
+++ pages/auth/index.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="auth">
+		<div class="auth__img">
+			<img src="~/public/img/authbg.jpg" />
+		</div>
+		<div class="auth__form"></div>
+	</div>
+</template>
+<script setup>
+definePageMeta({
+	layout: 'auth',
+})
+</script>
+<style lang="scss"></style>
