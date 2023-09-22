576f567e138f7ccc1e18de7b523adbcccdde784b
Muhammadshoh01 <rajabovmuhammadshoh009@gmail.com>
2023-09-22 23:16:31 +0500

    make work enter button

--- components/header.vue
+++ components/header.vue
@@ -3,7 +3,10 @@
 		<nuxt-link to="/" class="logo">
 			<img src="/img/logo.png" />
 		</nuxt-link>
-		<ul class="links">
+		<div class="menu" @click="menu__toggle = !menu__toggle">
+			<span></span>
+		</div>
+		<ul :class="`links ${menu__toggle ? 'active' : ''}`">
 			<li>
 				<nuxt-link to="/">Bosh sahifa</nuxt-link>
 			</li>
@@ -27,21 +30,87 @@
 			</li>
 		</ul>
 
-		<button class="search">
+		<button class="search" @click="searchToggle = !searchToggle">
 			<img src="/img/search-icon.png" />
 		</button>
-		<div class="lang">
-			<div class="current"><img src="/img/global-search.svg" /> O'zbekcha</div>
+		<div
+			:class="`lang ${language__toggle ? 'open' : ''}`"
+			@mouseenter="language__toggle = true"
+			@mouseleave="language__toggle = false"
+		>
+			<div class="current">
+				<img src="/img/global-search.svg" />
+				{{ langs[current_lang].title }}
+			</div>
 			<ul class="list">
-				<li>Русский</li>
-				<li>English</li>
+				<li
+					v-for="(lang, index) in langs"
+					:key="index"
+					@click="current_lang = index"
+				>
+					{{ lang.title }}
+				</li>
 			</ul>
 		</div>
+		<div class="search__content" v-if="searchToggle == true">
+			<form>
+				<input class="result" type="text" placeholder="Qidiruv..." />
+				<ul>
+					<li>
+						<nuxt-link>
+							Nodejs kursi
+							<span>750 000 so'm</span>
+						</nuxt-link>
+					</li>
+					<li>
+						<nuxt-link>
+							Nodejs kursi
+							<span>750 000 so'm</span>
+						</nuxt-link>
+					</li>
+					<li>
+						<nuxt-link>
+							Nodejs kursi
+							<span>750 000 so'm</span>
+						</nuxt-link>
+					</li>
+					<li>
+						<nuxt-link>
+							Nodejs kursi
+							<span>750 000 so'm</span>
+						</nuxt-link>
+					</li>
+				</ul>
+			</form>
+			<div class="search__close" @click="searchToggle = false"></div>
+		</div>
 		<div>
-			<nuxt-link to="/" class="login">Kirish</nuxt-link>
+			<nuxt-link to="/auth" class="login">Kirish</nuxt-link>
 		</div>
 	</header>
 </template>
+
+<script lang="ts" setup>
+const searchToggle = ref(false)
+const langs = ref([
+	{
+		title: "O'zbekcha",
+		slug: 'uz',
+	},
+	{
+		title: 'Русский',
+		slug: 'ru',
+	},
+	{
+		title: 'English',
+		slug: 'en',
+	},
+])
+
+const current_lang = ref(0)
+const language__toggle = ref(false)
+const menu__toggle = ref(false)
+</script>
 
 <style lang="scss">
 @import '../public/styles/css/header.scss';
