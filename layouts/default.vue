f6cf5fff3122005537afae8c460a6c8157b7fc7f
Muhammadshoh01 <rajabovmuhammadshoh009@gmail.com>
2023-09-22 23:10:58 +0500

    add layouts

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<headerPart />
+
+		<NuxtPage />
+
+		<footerPart />
+	</div>
+</template>
+<script setup>
+import headerPart from '~/components/header.vue'
+import footerPart from '~/components/footer.vue'
+</script>
+<style lang=""></style>
